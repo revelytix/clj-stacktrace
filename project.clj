6a6c2dc3285c3a23cf301bc2ea34c3f2ce80c934
Perry Trolard <trolard@gmail.com>
2012-08-24 11:46:42 -0500

    0.2.5-RVTX-1 release

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject clj-stacktrace "0.2.5-SNAPSHOT"
+(defproject clj-stacktrace "0.2.5-RVTX-1"
   :description "More readable stacktraces in Clojure programs."
   :url "http://github.com/mmcgrana/clj-stacktrace"
+  :jar-exclusions [#"test"]
   :dev-dependencies [[org.clojure/clojure "1.2.1"]
                      [lein-clojars "0.6.0"]])
